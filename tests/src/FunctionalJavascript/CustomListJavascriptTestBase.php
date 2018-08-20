eee0edef5c44e35bb0a8871bc2f358ae68e20fac
Mladen Todorovic <mladen.todorovic@burda.com>
2018-08-20 16:38:36 +0200

    Added test to create custom list block  with default source list plugin

--- tests/src/FunctionalJavascript/CustomListJavascriptTestBase.php
+++ tests/src/FunctionalJavascript/CustomListJavascriptTestBase.php
@@ -14,7 +14,13 @@ abstract class CustomListJavascriptTestBase extends WebDriverTestBase {
   /**
    * {@inheritdoc}
    */
-  protected static $modules = ['node', 'custom_list', 'views', 'entity_browser'];
+  protected static $modules = [
+    'node',
+    'block',
+    'views',
+    'layout_builder',
+    'entity_browser',
+  ];
 
   /**
    * {@inheritdoc}
@@ -22,78 +28,21 @@ abstract class CustomListJavascriptTestBase extends WebDriverTestBase {
   public function setUp() {
     parent::setUp();
 
-    $this->drupalCreateContentType(['type' => 'test']);
+    // Custom list requires article content type.
+    $this->drupalCreateContentType(['type' => 'article']);
+    $this->container->get('module_installer')->install(['custom_list']);
+
+    // The Layout Builder UI relies on local tasks.
+    $this->drupalPlaceBlock('local_tasks_block');
 
     $user = $this->drupalCreateUser([
       'access content',
-      'edit own test content',
-      'create test content',
+      'configure any layout',
+      'administer node display',
+      'add source list entities',
     ]);
 
     $this->drupalLogin($user);
-  }
-
-  /**
-   * Creates a new file field.
-   *
-   * @param string $name
-   *   The name of the new field (all lowercase), exclude the "field_" prefix.
-   * @param string $entity_type
-   *   The entity type.
-   * @param string $bundle
-   *   The bundle that this field will be added to.
-   * @param string $field_type
-   *   The field type.
-   * @param array $storage_settings
-   *   A list of field storage settings that will be added to the defaults.
-   * @param array $field_settings
-   *   A list of instance settings that will be added to the instance defaults.
-   * @param string $widget_type
-   *   The widget for the new field.
-   * @param array $widget_settings
-   *   A list of widget settings that will be added to the widget defaults.
-   *
-   * @throws \Drupal\Core\Entity\EntityStorageException
-   */
-  protected function createField($name, $entity_type, $bundle, $field_type, array $storage_settings = [], array $field_settings = [], $widget_type = 'string', array $widget_settings = []) {
-    $field_storage = FieldStorageConfig::create([
-      'entity_type' => $entity_type,
-      'field_name' => $name,
-      'type' => $field_type,
-      'settings' => $storage_settings,
-      'cardinality' => !empty($storage_settings['cardinality']) ? $storage_settings['cardinality'] : 1,
-    ]);
-    $field_storage->save();
-
-    $field = [
-      'field_name' => $name,
-      'label' => $name,
-      'entity_type' => $entity_type,
-      'bundle' => $bundle,
-      'required' => !empty($field_settings['required']),
-      'settings' => $field_settings,
-    ];
-    FieldConfig::create($field)->save();
-
-    entity_get_form_display($entity_type, $bundle, 'default')
-      ->setComponent($name, [
-        'type' => $widget_type,
-        'settings' => $widget_settings,
-      ])
-      ->save();
-  }
-
-  /**
-   * Selects an option in a select2 widget.
-   *
-   * @param string $field
-   *   Name of the field.
-   * @param array $keys
-   *   Values for the field.
-   */
-  protected function selectOption($field, array $keys) {
-    $this->getSession()->executeScript("jQuery('#$field').val(['" . implode("', '", $keys) . "'])");
-    $this->getSession()->executeScript("jQuery('#$field').trigger('change')");
   }
 
 }
